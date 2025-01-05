064590eb54ed0ba51cc65422df103bae07d863af
rahuvash <rahulvashisth052@gmail.com>
2025-01-06 02:00:57 +0530

    added 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,69 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { useSelector } from 'react-redux';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
-import ProductDetailsPage from './pages/ProductDetailsPage';
-import CartPage from './pages/CartPage';
-import Wishlist from './pages/Wishlist';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  useLocation,
+} from "react-router-dom";
+import { Provider } from "react-redux";
+import { useSelector } from "react-redux";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import ProductDetailsPage from "./pages/ProductDetailsPage";
+import CartPage from "./pages/CartPage";
+import Wishlist from "./pages/Wishlist";
+import NotFound from "./pages/NotFound";
 
-import Header from "./components/Header"
+import Header from "./components/Header";
 
 // Import updated PrivateRouteWrapper
-import store from './store';
-import PrivateRoute from './components/PrivateRoute';
-import AdminDashboard from './pages/AdminDashboard';
-
-
-
+import store from "./store";
+import PrivateRoute from "./components/PrivateRoute";
+import AdminDashboard from "./pages/AdminDashboard";
 
 const App = () => {
   const AppLayout = ({ children }) => {
     const location = useLocation(); // Access the current location
-  
+
     return (
       <>
         {/* Render Header only if the current path is not '/login' */}
-        {location.pathname !== '/login' && <Header />}
+        {location.pathname !== "/login" && <Header />}
         <main>{children}</main>
       </>
     );
   };
-  
-  
+
   return (
     <Provider store={store}>
       <Router>
-        <AppLayout/>
+        <AppLayout />
         <div className="App">
           <Routes>
             <Route
               path="/"
               element={
-                <PrivateRoute allowedRoles={['user','admin']} element={<HomePage />} />
+                <PrivateRoute
+                  allowedRoles={["user", "admin"]}
+                  element={<HomePage />}
+                />
               }
             />
             <Route
               path="/dashboard"
               element={
-                <PrivateRoute allowedRoles={['admin']} element={<AdminDashboard />}  />
+                <PrivateRoute
+                  allowedRoles={["admin"]}
+                  element={<AdminDashboard />}
+                />
               }
             />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/product/:id" element={<ProductDetailsPage />} />
             <Route path="/cart" element={<CartPage />} />
             <Route path="/wishlist" element={<Wishlist />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
       </Router>
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,14 @@
+// src/pages/NotFound.js
+
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="container mx-auto p-4 text-center">
+      <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+      <p className="mt-4">Sorry, the page you are looking for does not exist.</p>
+    </div>
+  );
+};
+
+export default NotFound;
